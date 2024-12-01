e66804b92d3d2f62a6e018e679f7ff60e62621a7
BlueNightFury46 <143189727+BlueNightFury46@users.noreply.github.com>
2024-12-01 14:50:46 +0000

    Update Main.java

--- src/main/java/dev/littleprogrammer/togglepvp/Main.java
+++ src/main/java/dev/littleprogrammer/togglepvp/Main.java
@@ -4,11 +4,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new pvp(), this);
         getCommand("pvpToggle").setExecutor(new Commands());
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TogglePvp] Plugin Loaded!");
+        this.saveDefaultConfig();
+
+        
     }
 
     @Override
