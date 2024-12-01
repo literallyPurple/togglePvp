279db1bbf30e474d00bfa93008071dc309e5088b
BlueNightFury46 <143189727+BlueNightFury46@users.noreply.github.com>
2024-12-01 18:20:50 +0000

    togglePvp fork that stores data beyond server restarts (#1)
    * Create config.yml
    
    * Update Main.java
    
    * Add new configuration section to store pvp mode
    
    * Update plugin.yml
    
    * Update config.yml
    
    * Added code that saves the pvpToggle status after a restart
    
    * Updated README
    
    * Fixed a silly always true error within the save config area
    
    * Updated plugin version
    
    * Added new aliases
    
    * Delete README.md

--- src/main/java/dev/littleprogrammer/togglepvp/Commands.java
+++ src/main/java/dev/littleprogrammer/togglepvp/Commands.java
@@ -4,28 +4,33 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nullable;
+
 public class Commands implements CommandExecutor {
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players can use that command!");
-            return true;
-        }
 
-        Player player = (Player) sender;
+
         if (label.equalsIgnoreCase("pvpToggle")) {
             if (!sender.isOp() && !sender.hasPermission("pvp.cmd")) {
                 sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
                 return true;
             } else {
                 if (pvp.onOff == true) {
+
                     sender.sendMessage(ChatColor.RED + "Pvp is on!");
                     pvp.onOff = false;
+                    return true;
                 } else {
+
                     sender.sendMessage(ChatColor.GREEN + "Pvp is off!");
                     pvp.onOff = true;
+                    return true;
                 }
             }
         }
--- src/main/java/dev/littleprogrammer/togglepvp/Main.java
+++ src/main/java/dev/littleprogrammer/togglepvp/Main.java
@@ -4,16 +4,28 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new pvp(), this);
         getCommand("pvpToggle").setExecutor(new Commands());
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TogglePvp] Plugin Loaded!");
+        this.saveDefaultConfig();
+        try {
+            pvp.onOff = this.getConfig().getBoolean("internal-data.pvp-off");
+        } catch (NullPointerException e){
+            this.getLogger().severe("FATAL ERROR: INTERNAL-DATA.PVP-OFF RETURNED NULL");
+        }
+
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "[TogglePvp] Plugin Shutting down!");
+        getConfig().set("internal-data.pvp-off", pvp.onOff);
+        saveConfig();
+
     }
 }
